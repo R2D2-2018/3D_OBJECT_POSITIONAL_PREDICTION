b417133b77be256ecf8917e78905452d2e9f6045
Danielvv1998 <daniel.vanvliet@student.hu.nl>
2018-05-10 14:27:32 +0200

    Added doxygen for getter

--- src/object-trajectory-predictor.hpp
+++ src/object-trajectory-predictor.hpp
@@ -3,16 +3,23 @@
 
 #include "vector3d.hpp"
 
+
 class ObjectTrajectoryPredictor {
 private:
     Vector3D position;
 public:
     ObjectTrajectoryPredictor();
 
+    /**
+     * @brief
+     * 
+     * This function is a getter for the position of the tracked object.
+     * 
+     * @return The current position of the object as Vector3D.
+     */
     Vector3D getPosition();
 
 };
-
 
 
 #endif // OBJECTTRAJECTORYPREDICTOR_HPP
