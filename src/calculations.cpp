19125f10e1dda251510710e67752a58109af75dd
Max Beunk <mbeunk@live.nl>
2018-05-14 12:49:07 +0200

    Changed input check of measurementDelay

--- src/calculations.cpp
+++ src/calculations.cpp
@@ -5,7 +5,7 @@ Calculations::Calculations() {
 }
 Vector3D Calculations::calculatePositionAfterMs(Vector3D position, Vector3D speed, uint32_t ms, uint32_t measurementDelay) {
     int x, y, z;
-    if (measurementDelay != 0) {
+    if (measurementDelay > 0) {
         x = position.getX() + (speed.getX() * (ms / measurementDelay));
         y = position.getY() + (speed.getY() * (ms / measurementDelay));
         z = position.getZ() + (speed.getZ() * (ms / measurementDelay));
