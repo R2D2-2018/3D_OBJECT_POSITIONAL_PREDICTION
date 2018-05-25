91d84f5434cb4c48c476447c8baa56206b200de4
Danielvv1998 <daniel.vanvliet@student.hu.nl>
2018-05-25 18:26:11 +0200

    updated test main to include the new operators

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "src/object_trajectory_predictor.hpp"
-#include "src/vector3d.hpp"
+#include "object_trajectory_predictor.hpp"
+#include "vector3d.hpp"
 
 TEST_CASE("Example Test Case") {
     REQUIRE(10 == 10);
@@ -18,16 +18,16 @@ TEST_CASE("ObjectTrajectoryPredictor::CalculatePositionAfterMs() returns correct
     REQUIRE(tempPredictor.calculatePositionAfterMs(Vector3D(2, 3, 5), Vector3D(1, 0, 0), 500, 1000) == Vector3D(3, 3, 5));
     REQUIRE(tempPredictor.calculatePositionAfterMs(Vector3D(2, 3, 5), Vector3D(2, 0, 0), 500, 1000) == Vector3D(3, 3, 5));
 }
+
 TEST_CASE("ObjectTrajectoryPredictor getPosition() returns a Vector3D") {
     auto tempPredictor = ObjectTrajectoryPredictor();
-
     REQUIRE(tempPredictor.getPosition().getX() == 0);
 }
 
 TEST_CASE("ObjectTrajectoryPredictor calculateSpeed() Test Case") {
     auto tempPredictor = ObjectTrajectoryPredictor();
-    const auto pos_1 = Vector3D(0, 0, 0);
-    const auto pos_2 = Vector3D(10, 15, 20);
+    auto pos_1 = Vector3D(0, 0, 0);
+    auto pos_2 = Vector3D(10, 15, 20);
     auto speed = tempPredictor.calculateSpeed(pos_1, pos_2, 1500);
     REQUIRE(speed.getX() == 6);
     REQUIRE(speed.getY() == 10);
@@ -44,6 +44,51 @@ TEST_CASE("ObjectTrajectoryPredictor calculateSpeed() Test Case") {
     REQUIRE(speed.getX() == 0);
     REQUIRE(speed.getY() == 0);
     REQUIRE(speed.getZ() == 0);
+}
 
+TEST_CASE("Vector3D operator== and operator !=") {
+    auto vector_1 = Vector3D(1, 2, 3);
+    auto vector_2 = Vector3D(1, 2, 3);
+    REQUIRE(vector_1 == vector_2);
+    REQUIRE(vector_1 != Vector3D(2, 2, 3));
+}
+TEST_CASE("Vector3D operator+=, operator-=, operator*=, operator/=") {
+    auto vector_1 = Vector3D(1, 2, 3);
+    vector_1 += Vector3D(1, 2, 3);
+    REQUIRE(vector_1 == Vector3D(2, 4, 6));
+    vector_1 += Vector3D(-1, -2, -3);
+    REQUIRE(vector_1 == Vector3D(1, 2, 3));
 
+    vector_1 *= Vector3D(10, 5, 3);
+    REQUIRE(vector_1 == Vector3D(10, 10, 9));
+    vector_1 *= Vector3D(-1, -2, -3);
+    REQUIRE(vector_1 == Vector3D(-10, -20, -27));
+
+    auto vector_2 = Vector3D(0, 0, 0);
+    vector_2 -= Vector3D(5, 10, 2);
+    REQUIRE(vector_2 == Vector3D(-5, -10, -2));
+    vector_2 -= Vector3D(-11, -16, -8);
+    REQUIRE(vector_2 == Vector3D(6, 6, 6));
+
+    vector_1 = Vector3D(5, 5, 5);
+    vector_1 /= Vector3D(5, 5, 5);
+    REQUIRE(vector_1 == Vector3D(1, 1, 1));
+    vector_1 /= Vector3D();
+    REQUIRE(vector_1 == Vector3D(1, 1, 1));
+    vector_1 /= Vector3D(-1, 1, -1);
+    REQUIRE(vector_1 == Vector3D(-1, 1, -1));
+}
+
+TEST_CASE("Vector3D operator+, operator-, operator*, operator/") {
+    auto vector_1 = Vector3D(0, 0, 0) + Vector3D(1, 2, 3);
+    REQUIRE(vector_1 == Vector3D(1, 2, 3));
+
+    vector_1 = Vector3D(10, 15, 20) - Vector3D(15, 20, 10);
+    REQUIRE(vector_1 == Vector3D(-5, -5, 10));
+
+    auto vector_2 = Vector3D(1, 2, 3) * Vector3D(5, 10, 20);
+    REQUIRE(vector_2 == Vector3D(5, 20, 60));
+
+    vector_1 = Vector3D(10, 20, 30) / Vector3D(2, 5, 6);
+    REQUIRE(vector_1 == Vector3D(5, 4, 5));
 }
